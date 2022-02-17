f64b37a8e70f4aaaa0d9395f6a0937cd441efb20
Teten Nugraha <teten.nugraha18@gmail.com>
2022-02-17 09:00:40 +0700

    install template bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import Menu from './components/Menu';
+import User from './pages/Users';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Nav />
+
+      <div className="container-fluid">
+        <div className="row">
+          
+          <Menu />
+
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+
+            <User />
+
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
